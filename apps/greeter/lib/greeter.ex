b6062b80595643efcdc919d0a118125ea82ce5f4
Julius Dejon <julius.dejon@shoresuite.com>
2019-03-29 14:03:50 +0800

    mix new greeter

---
+++ apps/greeter/lib/greeter.ex
@@ -0,0 +1,18 @@
+defmodule Greeter do
+  @moduledoc """
+  Documentation for Greeter.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Greeter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
